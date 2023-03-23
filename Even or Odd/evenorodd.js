b45f2f785060cb6c4dbc84d56e54a0a334fae8df
zain <mzainkhan2005@gmail.com>
2023-03-23 16:14:44 +0500

    second project

---
+++ Even or Odd/evenorodd.js
@@ -0,0 +1,7 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// Let's use a ternary operator
+const even_or_odd = (number) => {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+};
+console.log(even_or_odd(0)); // 'Even'
+console.log(even_or_odd(0)); // 'Even'
