da89ff0062bb640b9339930cc5d82a3631f842e1
zain <mzainkhan2005@gmail.com>
2023-03-24 13:31:50 +0500

    third project reuring string

---
+++ Returning Strings/file.js
@@ -0,0 +1,5 @@
+// Write a function that given the input string name, returns the greeting statement Hello, <name> how are you doing today?
+const greet = (name) => {
+  return `"Hello, ${name} how are you doing today?"`;
+};
+console.log(greet('zain'));
