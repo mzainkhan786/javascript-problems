594c6cb19c28682f90a562878f545dccd32295f2
zain <mzainkhan2005@gmail.com>
2023-03-30 16:43:58 +0500

    Sort an array from lowest to highest

--- Create a function that filters out negative numbers/file.js
+++ Create a function that filters out negative numbers/file.js
@@ -1,2 +1,9 @@
-var arr = [0, 1, 2, 3].reverse();
-         document.write("Reversed array is : " + arr ); 
+let numberArray = [12, -26, 32, 14, 27, -31, -17, 0, -1, 10];
+
+let result = numberArray.filter(testFunction);
+
+function testFunction(values) {
+  return values < 0;
+}
+
+console.log('Array of Negative Numbers: ', result);
