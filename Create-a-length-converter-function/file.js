26e07dc809cfd2549bfbde8e11571c00600dc256
zain <mzainkhan2005@gmail.com>
2023-03-30 15:13:04 +0500

    lenght-converter-function

---
+++ Create-a-length-converter-function/file.js
@@ -0,0 +1,7 @@
+let km_to_meter = function (km) {
+  let meter = km * 1000;
+  console.log(meter);
+};
+km_to_meter(2);
+
+// https://www.codecademy.com/resources/blog/10-javascript-code-challenges-for-beginners/
