42de75b44bedf62e892d19f69b630f23680c2b8a
zain <mzainkhan2005@gmail.com>
2023-03-31 16:20:59 +0500

    Return the number of vowels in a string

---
+++ Return the number of vowels in a string/file.js
@@ -0,0 +1,20 @@
+function returnvovel(value) {
+  let vowelsCount = 0;
+  const string = value.toString();
+  //   console.log(typeof value);
+  for (let i = 0; i <= string.length - 1; i++) {
+    //if a vowel, add to vowel count
+    if (
+      string.charAt(i) == 'a' ||
+      string.charAt(i) == 'e' ||
+      string.charAt(i) == 'i' ||
+      string.charAt(i) == 'o' ||
+      string.charAt(i) == 'u'
+    ) {
+      vowelsCount += 1;
+    }
+  }
+  console.log(vowelsCount);
+  return vowelsCount;
+}
+returnvovel('mme');
